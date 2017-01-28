8d59c93dfd1abb179e024e013cc356c1cc8e80fe
spences10 <spences10apps@gmail.com>
2017-01-28 13:07:28 +0000

    Add subQuery and responses

--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -4,8 +4,24 @@ module.exports = {
         '',
         ''
     ],
-    blackListUsers: [
+    queryStringSubQuery: [
+        '',
+        '',
         ''
     ],
-    resultType: ['mixed', 'recent', 'popular']
+    resultType: [
+        'mixed',
+        'recent',
+        'popular'
+    ],
+    responseString: [
+        'Hi @screenName thanks for the follow! !CR',
+        '@screenName thanks for following! !CR',
+        'Hey @screenName thanks for the follow! !CR',
+        'Thanks for following @screenName! !CR',
+        'Hey @screenName, thanks for following! !CR',
+        'Awesome @screenName, thanks for following! !CR',
+        'Thanks for the follow @screenName! !CR',
+        'Thanks for following @screenName! How are you today? !CR'
+    ]
 };
