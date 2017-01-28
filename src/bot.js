2b607c8c67b5faf47be455c3d1e5391c29a370c8
spences10 <spences10apps@gmail.com>
2017-01-28 18:54:10 +0000

    Add responses use

--- src/bot.js
+++ src/bot.js
@@ -13,7 +13,9 @@ var favoriteFrequency = 5
 // RANDOM QUERY STRING  =========================
 
 var qs = ura(strings.queryString)
+var qsSq = ura(strings.queryStringSubQuery)
 var rt = ura(strings.resultType)
+var rs = ura(strings.responseString)
 
 // https://dev.twitter.com/rest/reference/get/search/tweets
 // A UTF-8, URL-encoded search query of 500 characters maximum, including operators.
@@ -28,29 +30,31 @@ var rt = ura(strings.resultType)
 // * recent : return only the most recent results in the response
 // * popular : return only the most popular results in the response.
 
-var retweet = function () {
+var retweet = function() {
     var paramQS = qs()
+    paramQS += qsSq()
     var paramRT = rt()
     var params = {
         q: paramQS,
         result_type: paramRT,
         lang: 'en'
-    }
-    Twitter.get('search/tweets', params, function (err, data) {
+    };
+    Twitter.get('search/tweets', params, function(err, data) {
         // if there no errors
         if (!err) {
             // grab ID of tweet to retweet
             try {
                 // try get tweet id, derp if not
                 var retweetId = data.statuses[0].id_str
-            } catch (e) {
-                console.log('retweetId DERP! ', e.message)
-                return
+            }
+            catch (e) {
+                console.log('retweetId DERP! ', e.message, ' Query String: ' + paramQS)
+                return;
             }
             // Tell TWITTER to retweet
             Twitter.post('statuses/retweet/:id', {
                 id: retweetId
-            }, function (err, response) {
+            }, function(err, response) {
                 if (response) {
                     console.log('RETWEETED!', ' Query String: ' + paramQS)
                 }
@@ -58,24 +62,24 @@ var retweet = function () {
                 if (err) {
                     console.log('RETWEET ERROR! Duplication maybe...: ', err, ' Query String: ' + paramQS)
                 }
-            })
+            });
         }
         // if unable to Search a tweet
         else {
             console.log('Something went wrong while SEARCHING...')
         }
-    })
+    });
 }
 
 // retweet on bot start
 retweet()
-// retweet in every x minutes
+    // retweet in every x minutes
 setInterval(retweet, 60000 * retweetFrequency)
 
 // FAVORITE BOT====================
 
 // find a random tweet and 'favorite' it
-var favoriteTweet = function () {
+var favoriteTweet = function() {
     var paramQS = qs()
     paramQS += qsSq()
     var paramRT = rt()
@@ -86,22 +90,23 @@ var favoriteTweet = function () {
     }
 
     // find the tweet
-    Twitter.get('search/tweets', params, function (err, data) {
+    Twitter.get('search/tweets', params, function(err, data) {
 
         // find tweets
-        var tweet = data.statuses
-        var randomTweet = ranDom(tweet) // pick a random tweet
+        var tweet = data.statuses;
+        var randomTweet = ranDom(tweet); // pick a random tweet
 
         // if random tweet exists
         if (typeof randomTweet != 'undefined') {
             // Tell TWITTER to 'favorite'
             Twitter.post('favorites/create', {
                 id: randomTweet.id_str
-            }, function (err, response) {
+            }, function(err, response) {
                 // if there was an error while 'favorite'
                 if (err) {
                     console.log('CANNOT BE FAVORITE... Error: ', err, ' Query String: ' + paramQS)
-                } else {
+                }
+                else {
                     console.log('FAVORITED... Success!!!', ' Query String: ' + paramQS)
                 }
             })
@@ -111,7 +116,7 @@ var favoriteTweet = function () {
 
 // favorite on bot start
 favoriteTweet()
-// favorite in every x minutes
+    // favorite in every x minutes
 setInterval(favoriteTweet, 60000 * favoriteFrequency)
 
 // STREAM API for interacting with a USER =======
@@ -126,29 +131,18 @@ stream.on('follow', followed)
 // ...trigger the callback
 function followed(event) {
     console.log('Follow Event now RUNNING')
-    // get USER's twitter handler (screen name)
-    var name = event.source.name
+        // get USER's twitter handle (screen name)
     var screenName = event.source.screen_name
-    var userID = event.source.id
 
     // CREATE RANDOM RESPONSE  ============================
-    var responseString = ura([
-        `Hi @${screenName} thanks for the follow! !CR`,
-        `@${screenName} thanks for following! !CR`,
-        `Hey @${screenName} thanks for the follow! !CR`,
-        `Thanks for following @${screenName}! !CR`,
-        `Thanks for following @${screenName}! I look forward to tweeting with you. !CR`,
-        `Hey @${screenName}, thanks for following! !CR`,
-        `Awesome @${screenName}, thanks for following! !CR`,
-        `Thanks for the follow @${screenName}! !CR`,
-        `Thanks for following @${screenName}! How are you today? !CR`
-    ])
+    var responseString = rs()
+    var find = 'screenName'
+    var regex = new RegExp(find, "g")
+    responseString = responseString.replace(regex, screenName)
 
     // function that replies back to every USER who followed for the first time
-    var tweetResponse = responseString()
-
-    console.log(tweetResponse)
-    tweetNow(tweetResponse)
+    console.log(responseString)
+    tweetNow(responseString)
 
 }
 
@@ -156,18 +150,20 @@ function followed(event) {
 function tweetNow(tweetTxt) {
     var tweet = {
         status: tweetTxt
-    }
+    };
 
     // HARCODE user name in and check before RT
     var n = tweetTxt.search(/@UserNameHere/i)
 
     if (n != -1) {
         console.log('TWEET SELF! Skipped!!')
-    } else {
-        Twitter.post('statuses/update', tweet, function (err, data, response) {
+    }
+    else {
+        Twitter.post('statuses/update', tweet, function(err, data, response) {
             if (err) {
                 console.log('Cannot Reply to Follower. ERROR!: ' + err)
-            } else {
+            }
+            else {
                 console.log('Reply to follower. SUCCESS!')
             }
         })
