158250edc3c65c84403ceecf677cdfc2b62033fa
spences10 <spences10apps@gmail.com>
2017-02-01 19:22:42 +0000

    js standard format

--- src/bot.js
+++ src/bot.js
@@ -30,94 +30,92 @@ var rs = ura(strings.responseString)
 // * recent : return only the most recent results in the response
 // * popular : return only the most popular results in the response.
 
-var retweet = function() {
-    var paramQS = qs()
-    paramQS += qsSq()
-    var paramRT = rt()
-    var params = {
-        q: paramQS + paramBls(),
-        result_type: paramRT,
-        lang: 'en'
-    };
-    Twitter.get('search/tweets', params, function(err, data) {
-        // if there no errors
-        if (!err) {
-            // grab ID of tweet to retweet
-            try {
-                // try get tweet id, derp if not
-                var retweetId = data.statuses[0].id_str
-            }
-            catch (e) {
-                console.log('retweetId DERP! ', e.message, ' Query String: ' + paramQS)
-                return;
-            }
-            // Tell TWITTER to retweet
-            Twitter.post('statuses/retweet/:id', {
-                id: retweetId
-            }, function(err, response) {
-                if (response) {
-                    console.log('RETWEETED!', ' Query String: ' + paramQS)
-                }
-                // if there was an error while tweeting
-                if (err) {
-                    console.log('RETWEET ERROR! Duplication maybe...: ', err, ' Query String: ' + paramQS)
-                }
-            });
+var retweet = function () {
+  var paramQS = qs()
+  paramQS += qsSq()
+  var paramRT = rt()
+  var params = {
+    q: paramQS + paramBls(),
+    result_type: paramRT,
+    lang: 'en'
+  }
+  Twitter.get('search/tweets', params, function (err, data) {
+    // if there no errors
+    if (!err) {
+      // grab ID of tweet to retweet
+      try {
+        // try get tweet id, derp if not
+        var retweetId = data.statuses[0].id_str
+      } catch (e) {
+        console.log('retweetId DERP! ', e.message, ' Query String: ' + paramQS)
+        return
+      }
+      // Tell TWITTER to retweet
+      Twitter.post('statuses/retweet/:id', {
+        id: retweetId
+      }, function (err, response) {
+        if (response) {
+          console.log('RETWEETED!', ' Query String: ' + paramQS)
         }
-        // if unable to Search a tweet
-        else {
-            console.log('Something went wrong while SEARCHING...')
+        // if there was an error while tweeting
+        if (err) {
+          console.log('RETWEET ERROR! Duplication maybe...: ', err, ' Query String: ' + paramQS)
         }
-    });
+      })
+    }
+    // if unable to Search a tweet
+    else {
+      console.log('Something went wrong while SEARCHING...')
+    }
+  })
 }
 
 // retweet on bot start
 retweet()
-    // retweet in every x minutes
+// retweet in every x minutes
 setInterval(retweet, 60000 * retweetFrequency)
 
 // FAVORITE BOT====================
 
 // find a random tweet and 'favorite' it
-var favoriteTweet = function() {
-    var paramQS = qs()
-    paramQS += qsSq()
-    var paramRT = rt()
+var favoriteTweet = function () {
+  var paramQS = qs()
+  paramQS += qsSq()
+  var paramRT = rt()
 
-    var params = {
-        q: paramQS + paramBls(),
-        result_type: paramRT,
-        lang: 'en'
-    }
+  var params = {
+    q: paramQS + paramBls(),
+    result_type: paramRT,
+    lang: 'en'
+  }
 
-    // find the tweet
-    Twitter.get('search/tweets', params, function(err, data) {
+  // find the tweet
+  Twitter.get('search/tweets', params, function (err, data) {
 
-        // find tweets
-        var tweet = data.statuses;
-        var randomTweet = ranDom(tweet); // pick a random tweet
+    // find tweets
+    var tweet = data.statuses
+    var randomTweet = ranDom(tweet) // pick a random tweet
 
-        // if random tweet exists
-        if (typeof randomTweet != 'undefined') {
-            // Tell TWITTER to 'favorite'
-            Twitter.post('favorites/create', {
-                id: randomTweet.id_str
-            }, function(err, response) {
-                // if there was an error while 'favorite'
-                if (err) {
-                    console.log('CANNOT BE FAVORITE... Error: ', err, ' Query String: ' + paramQS)
-                }
-                else {
-                    console.log('FAVORITED... Success!!!', ' Query String: ' + paramQS)
-                }
-            })
+    // if random tweet exists
+    if (typeof randomTweet != 'undefined') {
+      // Tell TWITTER to 'favorite'
+      Twitter.post('favorites/create', {
+        id: randomTweet.id_str
+      }, function (err, response) {
+        // if there was an error while 'favorite'
+        if (err) {
+          console.log('CANNOT BE FAVORITE... Error: ', err, ' Query String: ' + paramQS)
+        }else {
+          console.log('FAVORITED... Success!!!', ' Query String: ' + paramQS)
         }
-    })
+      })
+    }
+  })
 }
 
 // favorite on bot start
 favoriteTweet()
-    // favorite in every x minutes
+// favorite in every x minutes
 setInterval(favoriteTweet, 60000 * favoriteFrequency)
 
 // STREAM API for interacting with a USER =======
@@ -130,59 +128,56 @@ var stream = Twitter.stream('user')
 stream.on('follow', followed)
 
 // ...trigger the callback
-function followed(event) {
-    console.log('Follow Event now RUNNING')
-        // get USER's twitter handle (screen name)
-    var screenName = event.source.screen_name
+function followed (event) {
+  console.log('Follow Event now RUNNING')
+  // get USER's twitter handle (screen name)
+  var screenName = event.source.screen_name
 
-    // CREATE RANDOM RESPONSE  ============================
-    var responseString = rs()
-    var find = 'screenName'
-    var regex = new RegExp(find, "g")
-    responseString = responseString.replace(regex, screenName)
+  // CREATE RANDOM RESPONSE  ============================
+  var responseString = rs()
+  var find = 'screenName'
+  var regex = new RegExp(find, 'g')
+  responseString = responseString.replace(regex, screenName)
 
-    // function that replies back to every USER who followed for the first time
-    console.log(responseString)
-    tweetNow(responseString)
-
+  // function that replies back to every USER who followed for the first time
+  console.log(responseString)
+  tweetNow(responseString)
 }
 
 // function definition to tweet back to USER who followed
-function tweetNow(tweetTxt) {
-    var tweet = {
-        status: tweetTxt
-    };
+function tweetNow (tweetTxt) {
+  var tweet = {
+    status: tweetTxt
+  }
 
-    // HARCODE user name in and check before RT
-    var n = tweetTxt.search(/@UserNameHere/i)
+  // HARCODE user name in and check before RT
+  var n = tweetTxt.search(/@UserNameHere/i)
 
-    if (n != -1) {
-        console.log('TWEET SELF! Skipped!!')
-    }
-    else {
-        Twitter.post('statuses/update', tweet, function(err, data, response) {
-            if (err) {
-                console.log('Cannot Reply to Follower. ERROR!: ' + err)
-            }
-            else {
-                console.log('Reply to follower. SUCCESS!')
-            }
-        })
-    }
+  if (n != -1) {
+    console.log('TWEET SELF! Skipped!!')
+  }else {
+    Twitter.post('statuses/update', tweet, function (err, data, response) {
+      if (err) {
+        console.log('Cannot Reply to Follower. ERROR!: ' + err)
+      }else {
+        console.log('Reply to follower. SUCCESS!')
+      }
+    })
+  }
 }
 
 // function to generate a random tweet tweet
-function ranDom(arr) {
-    var index = Math.floor(Math.random() * arr.length)
-    return arr[index]
+function ranDom (arr) {
+  var index = Math.floor(Math.random() * arr.length)
+  return arr[index]
 }
 
-function paramBls() {
-    var ret = '',
-        arr = strings.blockedStrings,
-        i, n
-    for (i = 0, n = arr.length; i < n; i++) {
-        ret += ' -' + arr[i];
-    }
-    return ret;
+function paramBls () {
+  var ret = '',
+    arr = strings.blockedStrings,
+    i, n
+  for (i = 0, n = arr.length; i < n; i++) {
+    ret += ' -' + arr[i]
+  }
+  return ret
 }
