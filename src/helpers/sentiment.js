0a0fa5adb96c629d878e78f197cece8f09f90d07
Scott Spence <spences10apps@gmail.com>
2017-02-03 16:32:54 +0000

    remove unused code, apply standard

--- src/helpers/sentiment.js
+++ src/helpers/sentiment.js
@@ -1,5 +1,4 @@
 const unirest = require('unirest')
-const fs = require('fs')
 
 /*
   Get a new API key at https://market.mashape.com/vivekn/sentiment-3
@@ -12,22 +11,9 @@ var sentiment = {}
 
 sentiment.init = function () {
   return unirest.post('https://community-sentiment.p.mashape.com/text/')
-    .header('X-Mashape-Key', apiKey)
-    .header('Content-Type', 'application/x-www-form-urlencoded')
-    .header('Accept', 'application/json')
-}
-
-sentiment.randomQuote = function () {
-  // Get content from file
-  var contents = fs.readFileSync('quotes.json')
-
-  // Define to JSON type
-  var jsonContent = JSON.parse(contents)
-
-  // Random number
-  var randomIndex = Math.floor(Math.random() * jsonContent.quotes.length)
-
-  return jsonContent.quotes[randomIndex]
+  .header('X-Mashape-Key', apiKey)
+  .header('Content-Type', 'application/x-www-form-urlencoded')
+  .header('Accept', 'application/json')
 }
 
 module.exports = sentiment
