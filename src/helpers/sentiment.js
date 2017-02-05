c1056e4cf3f647cb8052b726b266461b3b7ad849
Will Hurley <wjhurley@users.noreply.github.com>
2017-02-04 19:15:19 -0800

    Update sentiment.js

--- src/helpers/sentiment.js
+++ src/helpers/sentiment.js
@@ -4,14 +4,11 @@ const unirest = require('unirest')
   Get a new API key at https://market.mashape.com/vivekn/sentiment-3
 */
 
-// Sentiment 3 Mashape API key
-var apiKey = 'QqtO3XbGhFmshEmKBxy58FqKLvG3p1rx61ijsnaHTstuRd3jp0'
-
 var sentiment = {}
 
 sentiment.init = function () {
   return unirest.post('https://community-sentiment.p.mashape.com/text/')
-  .header('X-Mashape-Key', apiKey)
+  .header('X-Mashape-Key', process.env.SENTIMENT3_KEY)
   .header('Content-Type', 'application/x-www-form-urlencoded')
   .header('Accept', 'application/json')
 }
