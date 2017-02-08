38d028c0e3eb56b2546915be7d2f556919383e57
spences10 <spences10apps@gmail.com>
2017-02-08 06:33:30 +0000

    Fix linting :memo:

--- src/config.js
+++ src/config.js
@@ -4,11 +4,11 @@
  * access_token
  * access_token_secret
  */
-require('dotenv').config();
+require('dotenv').config()
 
 module.exports = {
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-};
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+}
