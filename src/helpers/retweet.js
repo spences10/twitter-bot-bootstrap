02cdc9f74680ba058a20dc0b0a10b71f30b4d674
spences10 <spences10apps@gmail.com>
2017-04-15 10:36:45 +0100

    chore(broke RT): broke RT
    retweet functionality broken, not working trying several different ways
    
    BREAKING CHANGE: RT

--- src/helpers/retweet.js
+++ src/helpers/retweet.js
@@ -1,21 +1,43 @@
 import Twiter from 'twitter'
 import config from '../config'
 import searchId from './search'
+import params from './parameters'
 
 const client = new Twiter(config.twitter)
 
-const retweet = async () => {
-  let res
-  console.log(client)
-  try {
-    res = await client.post('statuses/retweet/:id', {
-      id: searchId()
-    })
-    console.log('RES :', res, 'CLIENT :', client)
-  } catch (err) {
-    console.log('ERR: ', err)
-  }
-  console.log('RES :', res)
-}
+// const retweet = async () => {
+//   let res
+//   try {
+//     res = await client.post(`statuses/retweet/${searchId}`)
+//   } catch (err) {
+//     console.log('ERR :', err)
+//   }
+//   console.log('RES :', res)
+// }
+
+// const retweet = () => {
+//   client.post(`statuses/retweet/${searchId()}`, (error, tweet, response) => {
+//     if (!error) {
+//       console.log('TWEET================', tweet)
+//     } else {
+//       console.log('TWEET: ', tweet)
+//       // console.log(JSON.stringify(response))
+//     }
+//   })
+// }
+
+// const retweet = () => {
+//   client.post('statuses/retweet/:id', {
+//     id: searchId
+//   }, function (err, response) {
+//     if (response) {
+//       console.log('RETWEETED!', ' Query String:', params)
+//     }
+//     // if there was an error while tweeting
+//     if (err) {
+//       console.log('RETWEET ERROR! Duplication maybe...:', err, 'Query String:', params)
+//     }
+//   })
+// }
 
 export default retweet
