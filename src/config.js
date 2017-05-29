176800e125493dc0ae9acae743ee74ab14100011
spences10 <spences10apps@gmail.com>
2017-05-29 10:28:53 +0100

    No sentiment!

--- src/config.js
+++ src/config.js
@@ -17,6 +17,5 @@ module.exports = {
     quote: process.env.TWITTER_QUOTE_RATE * 1000 * 60,
     searchCount: process.env.TWITTER_SEARCH_COUNT,
     randomReply: process.env.RANDOM_REPLY
-  },
-  sentiment_api_key: process.env.SENTIMENT_KEY
+  }
 }
