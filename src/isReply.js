7a1206d6502a14f43c49d798252941eb47d987b0
Nicole Sattler <nicole.sattler@live.com>
2017-10-27 00:24:12 +0000

    Added isReply helper function

---
+++ src/isReply.js
@@ -0,0 +1,12 @@
+const isReply = (tweet) => {
+     if (tweet.is_quote_status ||
+         tweet.retweeted_status ||
+         tweet.in_reply_to_status_id ||
+         tweet.in_reply_to_status_id_str ||
+         tweet.in_reply_to_user_id ||
+         tweet.in_reply_to_user_id_str ||
+         tweet.in_reply_to_screen_name)
+         return true;
+};
+
+module.exports = isReply;
