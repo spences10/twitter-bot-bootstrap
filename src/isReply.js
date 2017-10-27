bf491921d20d298022538b64af447faad60b8d5e
Nicole Sattler <nicole.sattler@live.com>
2017-10-27 00:26:31 +0000

    Added regex

--- src/isReply.js
+++ src/isReply.js
@@ -1,12 +1,15 @@
 const isReply = (tweet) => {
-     if (tweet.is_quote_status ||
-         tweet.retweeted_status ||
-         tweet.in_reply_to_status_id ||
-         tweet.in_reply_to_status_id_str ||
-         tweet.in_reply_to_user_id ||
-         tweet.in_reply_to_user_id_str ||
-         tweet.in_reply_to_screen_name)
-         return true;
+    var RT = /^RT/i;
+    
+    if (RT.test(tweet.text) ||
+        tweet.is_quote_status ||
+        tweet.retweeted_status ||
+        tweet.in_reply_to_status_id ||
+        tweet.in_reply_to_status_id_str ||
+        tweet.in_reply_to_user_id ||
+        tweet.in_reply_to_user_id_str ||
+        tweet.in_reply_to_screen_name)
+        return true;
 };
 
 module.exports = isReply;
